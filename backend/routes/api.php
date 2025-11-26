c095582990f4c47cbace5174b1643f514b54d667
AtamnaSlimane <atamnamohamedslimane@gmail.com>
2025-11-26 21:53:46 +0100

    pagination for lists,mylistings route,profile change

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,20 +1,25 @@
 <?php
 
+use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\ListingController;
 use App\Http\Controllers\UserController;
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', fn(Request $request) => $request->user());
-
+    //get user info
+    Route::get('/user', [UserController::class, 'index']);
+    //can only change 5 times in 60 minutes
+    Route::patch('/user', [UserController::class, 'update'])
+     ->middleware('throttle:5,60');
     Route::post('/logout', [LoginController::class, 'logout']);
-
     // Protected routes (require token)
-    Route::get('/home', [UserController::class, 'index']); //fetch all listings
-    Route::post('/create-list', [UserController::class, 'store']); // create a listing
+    Route::post('/listings', [ListingController::class, 'store']); // create a listing
+    Route::get('/listings/mine', [ListingController::class, 'myListings']); // show the user own listings
 });
 #public routes
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/register', [RegisterController::class, 'register']);
+Route::get('/listings', [ListingController::class, 'index']); //fetch all listings
