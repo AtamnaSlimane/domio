a2d2dd23fd0a0efa4c8e025351d86c57bd6451f0
SouhilOS05 <souhiltheman@gmail.com>
2025-11-28 23:54:44 +0100

    Sending Auth token with each axios request using interceptors

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,12 +2,20 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_LARAVEL_API_URL || "http://127.0.0.1:8000/api",
+  baseURL:
+    process.env.NEXT_PUBLIC_LARAVEL_API_URL || "http://127.0.0.1:8000/api",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
-    Authorization: `Bearer ${Cookies.get("token")}`,
   },
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = Cookies.get("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default axiosInstance;
